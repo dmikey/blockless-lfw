969773a20bb930216115fd9fd97ec1f08ee7c19a
Derek Anderson <dmikey@users.noreply.github.com>
2023-07-17 14:52:33 -0500

    CLEAN UP

--- go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/blocklessnetwork/local-first-gateway
-
-go 1.20
-
-require github.com/nlepage/go-wasm-http-server v1.1.0
-
-require github.com/nlepage/go-js-promise v1.0.0 // indirect
--- sw.js
+++
@@ -1,8 +0,0 @@
-importScripts(
-  "https://cdn.jsdelivr.net/gh/golang/go@go1.20.6/misc/wasm/wasm_exec.js"
-);
-
-const go = new Go();
-WebAssembly.instantiateStreaming(fetch("test.wasm"), go.importObject).then(
-  ({ instance }) => go.run(instance)
-);
